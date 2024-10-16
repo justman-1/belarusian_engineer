14e5207f866416e454ea49a45b40b19fe58eac7d
justman-1 <sharom573@gmail.com>
2024-10-16 14:54:25 +0300

    1

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -9,23 +9,25 @@ interface PropsType {
 
 export default function CardLink(props: PropsType) {
   return (
-    <Link
-      href={props.path}
-      style={{ textDecoration: "none" }}
-      onClick={(e) => {
-        e.preventDefault()
-        console.log(props)
-        if (props.allDiv) props.allDiv.style.opacity = "0"
-        window.scroll({
-          top: -5000,
-          behavior: "smooth",
-        })
-        setTimeout(() => {
-          window.location.href = props.path
-        }, 500)
-      }}
-    >
-      <div className={st.block}>{props.name}</div>
-    </Link>
+    <>
+      <Link
+        href={props.path}
+        style={{ textDecoration: "none" }}
+        onClick={(e) => {
+          e.preventDefault()
+          console.log(props)
+          if (props.allDiv) props.allDiv.style.opacity = "0"
+          window.scroll({
+            top: -5000,
+            behavior: "smooth",
+          })
+          setTimeout(() => {
+            window.location.href = props.path
+          }, 500)
+        }}
+      >
+        <div className={st.block}>{props.name}</div>
+      </Link>
+    </>
   )
 }
