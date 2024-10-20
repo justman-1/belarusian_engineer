344f4f46a78ccb72d28b5fe5414866f2f45e6328
justman-1 <sharom573@gmail.com>
2024-10-20 11:54:14 +0300

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next"
+import { ChakraProvider } from "@chakra-ui/react"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
     </html>
   )
 }
--- src/app/words/[section_name]/page.tsx
+++ src/app/words/[section_name]/page.tsx
@@ -1,7 +1,12 @@
+"use client"
 import Header from "@/components/Header"
 import { Russo_One } from "next/font/google"
+import st from "./words_section.module.scss"
+import Image from "next/image"
+import Link from "next/link"
+import { Text } from "@chakra-ui/react"
 
-const font = Russo_One({ subsets: ["latin"], weight: "400" })
+const font = Russo_One({ subsets: ["latin"], weight: "400", display: "swap" })
 
 export default function Words_Section(/* {
   params,
@@ -12,15 +17,18 @@ export default function Words_Section(/* {
     <div className={font.className}>
       <Header />
       <main>
-        <div
-          style={{
-            fontSize: "8vw",
-            marginTop: "20vw",
-            textAlign: "center",
-          }}
-        >
-          Тут будуць карткі
-        </div>
+        <Link href="/" style={{ textDecoration: "none" }}>
+          <div className={st.back}>
+            <Image
+              className={st.backImg}
+              src="/arrow_left.svg"
+              width={15}
+              height={15}
+              alt=""
+            />
+            <Text fontSize="xs">Да галоўнай старонцы</Text>
+          </div>
+        </Link>
       </main>
     </div>
   )
